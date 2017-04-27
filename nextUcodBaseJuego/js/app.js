503cd4e27e688d1064346bd7ee670da43fe3d07e
andresrev1988 <andres.revelo@icloud.com>
2017-04-26 19:27:10 -0500

    Carga dulces

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,7 +1,4 @@
-$( document ).ready(function() {
-  console.log( "document loaded" );
-  parpadear();
-});
+//PARPADEA TITULO
 var parpadear = function() {
     $('.main-titulo').animate({
       color: 'white',
@@ -11,3 +8,42 @@ var parpadear = function() {
         }, 100, parpadear);
     });
 }
+//FUNCION RANDOMICA
+function imgRandomica(){
+  var random = Math.floor(Math.random() * 4) + 1;
+  return ("image/"+random+".png");
+}
+
+//Agrega Candy
+function agregaCandy(elemento,imagen){
+  $(elemento).prepend("<img src="+imagen+">").draggable();
+}
+//inicializa el tablero
+function tablero(){
+  $(.panel-tablero).droppable();
+  for (var i=1; i<=7; i++){
+    var j = 0;
+    while(j < 7 ){
+      agregaCandy(".col-"+i,imgRandomica());
+      j++;
+    }
+  }
+
+}
+//llAMA A FUNCIONES
+$( document ).ready(function() {
+  console.log( "document loaded" );
+  tablero();
+  $("img").css({
+    "margin": "auto",
+    "width": "90%",
+    "height": "90px"});
+  parpadear();
+});
+
+/*  $('.col-1').prepend("<img src=image/1.png>")
+img{
+  margin: auto;
+  width: 100%;
+  height: 95px;
+}*/
