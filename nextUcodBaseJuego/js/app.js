c554fd45b593d388099efe306c313984ee326461
andresrev1988 <andres.revelo@icloud.com>
2017-04-27 22:45:13 -0500

    Identifica Dulces

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,10 +1,20 @@
 //PARPADEA TITULO
 var parpadear = function() {
     $('.main-titulo').animate({
-      color: 'white',
+      color: 'white'
     },100 , function(){
         $(this).animate({
             color: 'yellow'
+        }, 100, parpadear);
+    });
+}
+//PARPADEA IMAGEN
+var blinkIMG = function(indice) {
+$("img")[indice].animate({
+      opacity : '0'
+    },100 , function(){
+        $(this).animate({
+            opacity : '1'
         }, 100, parpadear);
     });
 }
@@ -15,20 +25,48 @@ function imgRandomica(){
 }
 
 //Agrega Candy
-function agregaCandy(elemento,imagen){
-  $(elemento).prepend("<img src="+imagen+">").draggable();
+function agregaCandy(elemento,imagen,posicion){
+  $(elemento).prepend("<img src="+imagen+" alt="+posicion+">");
 }
 //inicializa el tablero
 function tablero(){
-  $(.panel-tablero).droppable();
-  for (var i=1; i<=7; i++){
+  for (var i=0; i<8; i++){
     var j = 0;
     while(j < 7 ){
-      agregaCandy(".col-"+i,imgRandomica());
+      agregaCandy(".col-"+i,imgRandomica(),i+""+j);
       j++;
     }
   }
-
+}
+//Verifica y efecto 3 en linea
+function chequearColumna(seleccionado,columna){
+    var imgClick = $("img")[seleccionado].src;
+    var  inicio=columna*7;
+    var  fin =(columna*7) + 7;
+    var cont = 0;
+    for(var i = seleccionado ; i< fin ; i++){
+      var abajo = $("img")[i].src;
+      if(imgClick==abajo){
+        cont++;
+        blinkIMG(i);
+      }else {
+        break;
+      }
+    }
+}
+////////////////////////////////////////////////////////
+function chequearFila(seleccionado){
+    var imgClick = $("img")[seleccionado].src;
+    var cont = 0;
+    for(var i = seleccionado ; i < 49 ; (i=i+7)){
+      var abajo = $("img")[i].src;
+      if(imgClick==abajo){
+        cont++;
+        blinkIMG(i);
+      }else {
+        break;
+      }
+    }
 }
 //llAMA A FUNCIONES
 $( document ).ready(function() {
@@ -37,13 +75,16 @@ $( document ).ready(function() {
   $("img").css({
     "margin": "auto",
     "width": "90%",
-    "height": "90px"});
+    "height": "90px"
+  });
+
+  $("img").on("click", function() {
+    var indice =  $("img").index(this);
+    var columna=($(this).parent().attr('class'));
+    columna=columna.replace("col-","");
+    columna--;
+    chequearColumna (indice,columna);
+    chequearFila(indice);
+  });
   parpadear();
 });
-
-/*  $('.col-1').prepend("<img src=image/1.png>")
-img{
-  margin: auto;
-  width: 100%;
-  height: 95px;
-}*/
