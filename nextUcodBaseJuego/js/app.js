f998bc0122b120fd20dcfb836102f50daadb5148
andresrev1988 <andres.revelo@icloud.com>
2017-05-02 11:08:57 -0500

    Finalizado con timer

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,163 +1,250 @@
 //PARPADEA TITULO
-var parpadear = function() {
+function parpadear() {
     $('.main-titulo').animate({
       color: 'white'
-    },100 , function(){
-        $(this).animate({
-            color: 'yellow'
-        }, 100, parpadear);
+    },400 , function(){
+      $(this).animate({
+        color: 'yellow'
+      }, 400, parpadear);
     });
-}
-//FUNCION RANDOMICA
-function imgRandomica(){
-  var random = Math.floor(Math.random() * 4) + 1;
-  return ("image/"+random+".png");
-}
-
-//Agrega Candy
-function agregaCandy(fila,columna){
-  var imagen = imgRandomica();
-
-  $(".panel-tablero").find("div."+columna).prepend("<img src="+imagen+" id="+columna.replace("col-","")+"-"+fila+">");
-  $("img").css({
-    "margin": "auto",
-    "width": "90%",
-    "height": "90px"
-  });
-}
-//inicializa el tablero
-function tablero(){
-  $( "div.panel-tablero" ).children().each(function() {
-    var columna = $(this).attr('class');
-    for (var i=1; i<8; i++){
-        agregaCandy(i,columna);
-      }
-  });
-}
-//Arregla Indices columna
-function arreglaIndices(){
-  for(var i=1;i<8;i++){
-    var j = 7;
-    $("div.col-"+i).children().each(function(){
-        this.id=("col-"+i).replace("col-","")+"-"+j;
-        j--;
+  }
+//Parpadea Caramelo
+  function parpadearCandy(items) {
+    for (var i=0;i<items.length;i++){
+      $("#"+items[i]).animate({
+        opacity: '0.25',
+      },50,function(){
+        $(this).animate({
+          opacity: '1',
+        },50, parpadearCandy(items));
       });
     }
 }
-//Elimina Columnas y agrega nuevas
-function eliminaCandy(eliminar){
-    for (var i=0;i<eliminar.length;i++){
-      var item = eliminar[i];
-      var itemArray= item.split("");
-      var fila =itemArray[2];
-      var columna = itemArray[1];
-      $("img#"+columna+"-"+fila).remove();
-      agregaCandy(fila,"col-"+columna);
-    }      
-  arreglaIndices();
+//Detinene Parpadeo Caramelo
+function parpadearCandyStop(items) {
+  for (var i=0;i<items.length;i++){
+    $("#"+items[i]).clearQueue();
+    $("#"+items[i]).stop();
+  }
 }
-
-//Verifica y efecto 3 en linea vertucal
-function chequear(item){
-
-    var imgClick = $("#"+item).attr("src");
-    var itemArray = item.split("-");
-    var fila = itemArray[1];
-    var columna = itemArray[0];
-    //Chequea Verticalmente
-    var eliminarAbajo=[];
-    for(var i = fila ; i >= 1 ; i--){
-      var imagen = $("#"+columna+"-"+i).attr("src");
-      if(imgClick==imagen){
-        eliminarAbajo.push(("#"+columna+i));
-      }else {
-        break;
+//Agrega Estructura
+  function estructura(){
+    var cuadricula= [];
+    for(var i = 1; i<8; i++){
+      for(var j = 1; j<8; j++){
+        cuadricula.push(i+""+j);
       }
     }
-    console.log("abajo"+eliminarAbajo);
-    var eliminarArriba=[];
-    for(var i = fila ; i < 8 ; i++){
-      var imagen = $("#"+columna+"-"+i).attr("src");
-      if(imgClick==imagen){
-        eliminarArriba.push(("#"+columna+i));
-      }else {
-        break;
+    agregaCandy(cuadricula);
+  }
+  //Funcion para eliminar repetidos en un Array
+  function arrayUnique(array) {
+      var a = array.concat();
+      for(var i=0; i<a.length; ++i) {
+          for(var j=i+1; j<a.length; ++j) {
+              if(a[i] === a[j])
+                  a.splice(j--, 1);
+          }
       }
-    }
-    console.log("arriba"+eliminarArriba);
-    //Elimina Horizontales
-    var eliminarDerecha=[];
-    for(var i = columna ; i < 8 ; i++){
-      var imgen = $("#"+i+"-"+fila).attr("src");
-      if(imgClick==imgen){
-        eliminarDerecha.push(("#"+i+fila));
-      }else {
-        break;
-      }
-    }
-    console.log("derecha"+eliminarDerecha);
-    var eliminarIzquierda=[];
-    for(var i = columna ; i >= 1 ; i--){
-      var imgen = $("#"+i+"-"+fila).attr("src");
-      if(imgClick==imgen){
-        eliminarIzquierda.push(("#"+i+fila));
-      }else {
-        break;
-      }
-    }
-    console.log("izq"+eliminarIzquierda);
-    var eliminarVert=[];
-    var eliminarHorz=[];
-    eliminarHorz = arrayUnique(eliminarHorz.concat(eliminarIzquierda));
-    eliminarHorz = arrayUnique(eliminarHorz.concat(eliminarDerecha));
-    console.log(eliminarHorz);
-    if(eliminarHorz.length<3){
-      eliminarHorz=[];
-    }
-    eliminarVert = arrayUnique(eliminarVert.concat(eliminarArriba));
-    eliminarVert = arrayUnique(eliminarVert.concat(eliminarAbajo));
-    console.log(eliminarVert);
-    if(eliminarVert.length<3){
-      eliminarVert=[];
-    }
-    var eliminar = arrayUnique(eliminarVert.concat(eliminarHorz));
-    console.log(eliminar);
-    eliminaCandy(eliminar);
-}
-//Funcion para eliminar repetidos en un Array
-function arrayUnique(array) {
-    var a = array.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
-
-    return a;
-}
-//Funcion verifica lineas 3X3
-function ExisteLineas(){
-  var flag = true;
-  while(flag){
-    for (var i =1; i<8; i++){
-      $("div.col-"+i).children().each(function(){
-          flag=chequear(this.id);
-          console.log(flag);
+      return a;
+  }
+  //Reordena ID's
+  function arreglaIndices(){
+      for (var i =1; i<8;i++){
+        var j =7;
+        $("div.col-"+i).children().each(function(){
+          $(this).attr("id","item"+j+""+i);
+          $(this).children().attr("id",j+""+i);
+          j--;
         });
+      }
     }
+  //Verifica y efecto 3 en linea vertucal
+  function movimientoAcertado(item){
+      var eliminar = CHEKEADO(item);
+      setTimeout(function() {
+          parpadearCandy(eliminar);
+      }, 500);
+      parpadearCandyStop(eliminar);
+      eliminaCandy(eliminar);
+      agregaCandy(eliminar);
+      setTimeout(function() {
+          validaTablero();
+        }, 1500);
+
+  }
+  //Elimina Columnas y agrega nuevas
+  function eliminaCandy(items){
+    setTimeout(function() {
+        for (var i=0;i<items.length;i++){
+          $("#item"+items[i]).remove();
+          puntaje = puntaje +10;
+          $("#score-text").text(puntaje);
+        }
+      }, 1000);
+  }
+  //Agrega Candy
+  function agregaCandy(items){
+    setTimeout(function() {
+      for (var i=0;i<items.length;i++){
+        var itemArray = items[i].split("");
+        var fila = itemArray[0];
+        var columna = itemArray[1];
+        var random = Math.floor(Math.random() * 4) + 1;
+        var imagen = "image/"+random+".png";
+        $(".panel-tablero").find("div.col-"+columna).prepend("<div id=item"+items[i]+"><img id="+items[i]+" src="+imagen+"></div>");
+        $("#"+items[i]).draggable({
+          revert: true,
+          containment: ".panel-tablero",
+          drag: function(event, ui){}
+        });
+      $("#item"+items[i]).droppable({
+        drop: function(event, ui){
+          imagenUno = event.target.lastChild;
+          imagenDos = ui.draggable[0];
+          imgUno = $(imagenUno).attr("src");
+          imgDos = $(imagenDos).attr("src");
+          $(imagenUno).attr("src", imgDos);
+          $(imagenDos).attr("src", imgUno);
+          if(imgUno!=imgDos){
+            cont++;
+            $("#movimientos-text").text(cont);
+            movimientoAcertado ($(imagenUno).attr("id"));
+          }
+      }
+      });
+      $("img").css({
+        "margin": "auto",
+        "width": "90%",
+        "height": "90px"
+      });
+    }
+    arreglaIndices();
+  }, 1500);
+}
+
+//Valida tablero
+function validaTablero(){
+  var num = 0;
+  var uno = [];
+  for (var i=1; i<8;i++){
+    for (var j=1; j<8;j++){
+      uno=arrayUnique(uno.concat(CHEKEADO (i+""+j)));
+      }
+  }
+  if(uno.length>0){
+      setTimeout(function() {
+          parpadearCandy(uno);
+      }, 500);
+      parpadearCandyStop(uno);
+      eliminaCandy(uno);
+      agregaCandy(uno);
+      setTimeout(function() {
+          validaTablero();
+        }, 2000);
   }
 }
 
+//VAlIDA LINEAS
+function CHEKEADO(item){
+  var imgClick = $("#"+item).attr("src");
+  var itemArray = item.split("");
+  var fila = itemArray[0];
+  var columna = itemArray[1];
+  //Chequea Verticalmente
+  var eliminarAbajo=[];
+  for(var i = fila ; i >= 1 ; i--){
+    var imagen = $("#"+(i+columna)).attr("src");
+    if(imgClick==imagen){
+      eliminarAbajo.push((i+columna));
+    }else {
+      break;
+    }
+  }
+  var eliminarArriba=[];
+  for(var i = fila ; i < 8 ; i++){
+    var imagen = $("#"+(i+columna)).attr("src");
+    if(imgClick==imagen){
+      eliminarArriba.push((i+columna));
+    }else {
+      break;
+    }
+  }
+  //Elimina Horizontales
+  var eliminarDerecha=[];
+  for(var i = columna ; i < 8 ; i++){
+    var imgen = $("#"+(fila+i)).attr("src");
+    if(imgClick==imgen){
+      eliminarDerecha.push((fila+i));
+    }else {
+      break;
+    }
+  }
+  var eliminarIzquierda=[];
+  for(var i = columna ; i >= 1 ; i--){
+    var imgen = $("#"+(fila+i)).attr("src");
+    if(imgClick==imgen){
+      eliminarIzquierda.push((fila+i));
+    }else {
+      break;
+    }
+  }
+  var eliminarVert=[];
+  var eliminarHorz=[];
+  eliminarHorz = arrayUnique(eliminarHorz.concat(eliminarIzquierda));
+  eliminarHorz = arrayUnique(eliminarHorz.concat(eliminarDerecha));
+  if(eliminarHorz.length<3){
+    eliminarHorz=[];
+  }
+  eliminarVert = arrayUnique(eliminarVert.concat(eliminarArriba));
+  eliminarVert = arrayUnique(eliminarVert.concat(eliminarAbajo));
+  if(eliminarVert.length<3){
+    eliminarVert=[];
+  }
+  var eliminar = arrayUnique(eliminarVert.concat(eliminarHorz));
+  return eliminar;
+  }
+//Inicializa Juego
+function initialize(){
+  if(flag){
+    $(".panel-tablero").fadeIn("slow");
+    $(".time").fadeIn("slow");
+    $(".panel-score").css({
+      "width": "25%"
+    });
+  }
+  $("div[class^='col-']").empty();
+  estructura();
+  cont=0;
+  $("#movimientos-text").text("0");
+  puntaje = 0 ;
+  $("#score-text").text("0");
+  $("#timer").text("0:00");
+  $('#timer').timer({
+   duration: '20s',
+   format: '%M:%S',
+   callback: function(){
+      fin();
+      $('#timer').timer('remove');
+   }
+  });
+  flag=false;
+}
+//finaliza el Juego
+function fin(){
+  $(".panel-tablero").fadeOut("slow");
+  $(".time").fadeOut("slow");
+  $(".panel-score").css({
+    "width": "100%"
+  });
+  flag= true;
+}
 //llAMA A FUNCIONES
 $( document ).ready(function() {
   console.log( "document loaded" );
-  tablero();
-  $(".panel-tablero").on("click", function(event) {
-    var item = event.target.id;
-    chequear (item);
-  });
-
+  flag = false;
   parpadear();
-
+  $(".btn-reinicio").click(function(){
+      initialize();
+  });
 });
